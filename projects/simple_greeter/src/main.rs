a66fd1f61345946ee9495a3d49f14a6b011c3bd0
paintedman00 <paintedman00@users.noreply.github.com>
2026-01-01 04:36:33 +0000

    implement simple_greeter logic

---
+++ projects/simple_greeter/src/main.rs
@@ -0,0 +1,14 @@
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("Usage: simple_greeter <name>");
+        std::process::exit(1);
+    }
+
+    let name = &args[1];
+
+    println!("Hello, {}!", name);
+}
